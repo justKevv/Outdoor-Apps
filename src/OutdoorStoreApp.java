1288045a4c0aa6793919a04d3e61aa33992450a8
KhenCahyo13 <khencahyo02@gmail.com>
2024-11-23 13:45:19 +0700

    styling: Create Categories tab contents

---
+++ src/OutdoorStoreApp.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class OutdoorStoreApp {
+    private JTabbedPane mainTabPane;
+    private JPanel mainPanel;
+    private JPanel salesPanel;
+    private JPanel itemsPanel;
+    private JPanel categoriesPanel;
+    private JPanel revenuePanel;
+
+    // Categories Tab Attributes
+    private JLabel categoryFormTitle;
+    private JLabel categoryNameLabel;
+    private JTextField categoryNameField;
+    private JButton submitCategoryButton;
+    private JScrollPane categoriesTableSP;
+    private JTable categoriesTable;
+}
